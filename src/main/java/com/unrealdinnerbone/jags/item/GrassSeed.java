17006390e75e001fd267f685654806ca4c1285f5
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2021-08-19 16:28:19 -0500

    Made it remove seed

--- src/main/java/com/unrealdinnerbone/jags/item/GrassSeed.java
+++ src/main/java/com/unrealdinnerbone/jags/item/GrassSeed.java
@@ -1,10 +1,14 @@
 package com.unrealdinnerbone.jags.item;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.Tags;
 
 public class GrassSeed extends Item {
@@ -15,11 +19,17 @@ public class GrassSeed extends Item {
 
     @Override
     public ActionResultType useOn(ItemUseContext context) {
-        if(context.getLevel().getBlockState(context.getClickedPos()).is(Tags.Blocks.DIRT)) {
+        BlockState blockState = context.getLevel().getBlockState(context.getClickedPos());
+        if(blockState.is(Tags.Blocks.DIRT) && blockState.getBlock() != Blocks.GRASS_BLOCK) {
             context.getLevel().setBlockAndUpdate(context.getClickedPos(), Blocks.GRASS_BLOCK.defaultBlockState());
-            return ActionResultType.sidedSuccess(context.getLevel().isClientSide);
+            context.getPlayer().playSound(SoundEvents.GRASS_PLACE, 1, 1);
+            if (!context.getPlayer().abilities.instabuild) {
+                context.getItemInHand().shrink(1);
+            }
+            return ActionResultType.sidedSuccess(context.getLevel().isClientSide());
+        }else {
+            return ActionResultType.PASS;
         }
-        return ActionResultType.PASS;
     }
 
 }
